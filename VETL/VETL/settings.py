214a1e84e2522c0f0f9c67b97888866207d1b904
“Divya <divyasajjan@yahoo.co.in>
2026-02-12 15:16:11 -0500

    reverted the installed apps

--- VETL/VETL/settings.py
+++ VETL/VETL/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "VETL.Product",
+    "Product",
     "rest_framework",
     "corsheaders"
 ]
