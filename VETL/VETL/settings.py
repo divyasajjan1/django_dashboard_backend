09437e7b08f93437cc8dedbe3b0092e0edc50a95
“Divya <divyasajjan@yahoo.co.in>
2026-02-12 15:10:55 -0500

    Installed apps change

--- VETL/VETL/settings.py
+++ VETL/VETL/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "Product",
+    "VETL.Product",
     "rest_framework",
     "corsheaders"
 ]
