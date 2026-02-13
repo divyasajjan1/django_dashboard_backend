e2a28355894a53839c75a93f3c0b59be58eaa505
“Divya <divyasajjan@yahoo.co.in>
2026-02-13 15:06:08 -0500

    updated settings

--- VETL/VETL/settings.py
+++ VETL/VETL/settings.py
@@ -48,10 +48,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "Product",
     "rest_framework",
-    "corsheaders"
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -59,7 +60,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware"
 ]
 
 #ROOT_URLCONF = "VETL.urls"
